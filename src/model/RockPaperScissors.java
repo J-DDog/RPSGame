49cb8884b8b4a829b9b5ffe2d68ce856e1bb15df
Traitortots <jker3169@740s-pl-844.local>
2016-03-23 09:28:13 -0600

    added the logic of the RPS bot

--- src/model/RockPaperScissors.java
+++ src/model/RockPaperScissors.java
@@ -2,9 +2,9 @@ package model;
 
 public class RockPaperScissors
 {
-	
-	private RPSNum p1Choice;
-	private RPSNum p2Choice;
+	private int p1Wins;
+	private int p2Wins;
+	private int botWins;
 	private RPSNum botChoice;
 	
 	public RockPaperScissors()
@@ -14,15 +14,97 @@ public class RockPaperScissors
 	
 	public int playerScores(RPSNum p1Choice)
 	{
-		
-		return 0;
+		int winner = 0;
+		updateBotChoice();
+		if(p1Choice != botChoice)
+		{
+			switch(botChoice)
+			{
+				case Rock:
+					switch(p1Choice)
+					{
+						case Paper:
+							winner = 1;
+							break;
+						case Scissors:
+							winner = 2;
+							break;
+					}
+					break;
+				case Paper:
+					switch(p1Choice)
+					{
+						case Scissors:
+							winner = 1;
+							break;
+						case Rock:
+							winner = 2;
+							break;
+					}
+					break;
+				case Scissors:
+					switch(p1Choice)
+					{
+						case Rock:
+							winner = 1;
+							break;
+						case Paper:
+							winner = 2;
+							break;
+					}
+					break;
+					
+			}
+			
+		}
+		return winner;
 	}
 	
 	public int playerScores(RPSNum p1Choice, RPSNum p2Choice)
 	{
-		
-		
-		return 0;
+		int winner = 0;
+		if(p1Choice != p2Choice)
+		{
+			switch(p2Choice)
+			{
+				case Rock:
+					switch(p1Choice)
+					{
+						case Paper:
+							winner = 1;
+							break;
+						case Scissors:
+							winner = 2;
+							break;
+					}
+					break;
+				case Paper:
+					switch(p1Choice)
+					{
+						case Scissors:
+							winner = 1;
+							break;
+						case Rock:
+							winner = 2;
+							break;
+					}
+					break;
+				case Scissors:
+					switch(p1Choice)
+					{
+						case Rock:
+							winner = 1;
+							break;
+						case Paper:
+							winner = 2;
+							break;
+					}
+					break;
+					
+			}
+			
+		}
+		return winner;
 	}
 	
 	public void updateBotChoice()
