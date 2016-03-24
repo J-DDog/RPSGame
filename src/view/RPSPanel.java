bfbba15cb45fadbe5362b5c48e6ecde2bef8d486
Traitortots <jker3169@740s-pl-844.local>
2016-03-24 09:53:33 -0600

    YEAH OUTPUT THE WINNER AND THE SCORE

--- src/view/RPSPanel.java
+++ src/view/RPSPanel.java
@@ -40,6 +40,7 @@ public class RPSPanel extends JPanel
 	public RPSPanel(RPSController baseController)
 	{
 		this.baseController = baseController;
+		this.setFocusable(true);
 		baseLayout = new SpringLayout();
 		rockButton = new JButton("Rock");
 		baseLayout.putConstraint(SpringLayout.WEST, rockButton, 100, SpringLayout.WEST, this);
@@ -47,14 +48,17 @@ public class RPSPanel extends JPanel
 		scissorsButton = new JButton("Scissors");
 		baseLayout.putConstraint(SpringLayout.EAST, scissorsButton, -100, SpringLayout.EAST, this);
 		p1Score = new JTextField("P1:");
+		p1Score.setEditable(false);
 		baseLayout.putConstraint(SpringLayout.EAST, p1Score, -296, SpringLayout.EAST, scissorsButton);
 		baseLayout.putConstraint(SpringLayout.NORTH, p1Score, 10, SpringLayout.NORTH, this);
 		baseLayout.putConstraint(SpringLayout.WEST, p1Score, 100, SpringLayout.WEST, this);
 		tieScore = new JTextField("Tie:");
+		tieScore.setEditable(false);
 		baseLayout.putConstraint(SpringLayout.NORTH, tieScore, 0, SpringLayout.NORTH, p1Score);
 		baseLayout.putConstraint(SpringLayout.WEST, tieScore, 258, SpringLayout.WEST, this);
 		baseLayout.putConstraint(SpringLayout.EAST, tieScore, 0, SpringLayout.EAST, paperButton);
 		p2Score = new JTextField("Bot:");
+		p2Score.setEditable(false);
 		baseLayout.putConstraint(SpringLayout.NORTH, p2Score, 0, SpringLayout.NORTH, p1Score);
 		baseLayout.putConstraint(SpringLayout.WEST, p2Score, 46, SpringLayout.EAST, tieScore);
 		baseLayout.putConstraint(SpringLayout.EAST, p2Score, 150, SpringLayout.EAST, tieScore);
@@ -150,40 +154,81 @@ public class RPSPanel extends JPanel
 			@Override
 			public void keyTyped(KeyEvent keyE)
 			{
-				int key = keyE.getKeyCode();
-				
+				char key = keyE.getKeyChar();
 				switch(key)
 				{
-					case KeyEvent.VK_Z:
-						baseController.updatePlay(RPSNum.Rock, 1);
-						p1Updated = true;
+					case 'z':
+						if(!p1Updated)
+						{
+							baseController.updatePlay(RPSNum.Rock, 1);
+							p1Updated = true;
+							
+						}
 						break;
-					case KeyEvent.VK_X:
-						baseController.updatePlay(RPSNum.Paper, 1);
-						p1Updated = true;
+					case 'x':
+						if(!p1Updated)
+						{
+							baseController.updatePlay(RPSNum.Paper, 1);
+							p1Updated = true;
+						}
 						break;
-					case KeyEvent.VK_C:
-						baseController.updatePlay(RPSNum.Scissors, 1);
-						p1Updated = true;
+					case 'c':
+						if(!p1Updated)
+						{
+							baseController.updatePlay(RPSNum.Scissors, 1);
+							p1Updated = true;
+						}
 						break;
 						
-					case KeyEvent.VK_LEFT:
-						baseController.updatePlay(RPSNum.Rock, 2);
-						p2Updated = true;
+					case '1':
+						if(!p2Updated)
+						{
+							baseController.updatePlay(RPSNum.Rock, 2);
+							p2Updated = true;
+						}
 						break;
-					case KeyEvent.VK_DOWN:
-						baseController.updatePlay(RPSNum.Paper, 2);
-						p2Updated = true;
+					case '2':
+						if(!p2Updated)
+						{
+							baseController.updatePlay(RPSNum.Paper, 2);
+							p2Updated = true;
+						}
 						break;
-					case KeyEvent.VK_RIGHT:
-						baseController.updatePlay(RPSNum.Scissors, 2);
-						p2Updated = true;
+					case '3':
+						if(!p2Updated)
+						{
+							baseController.updatePlay(RPSNum.Scissors, 2);
+							p2Updated = true;
+						}
 						break;
 					default:
-						
 						break;
+						
 				}
 				
+				if(p1Updated && p2Updated)
+				{
+					switch(baseController.getWinner(true))
+					{
+						case 0:
+							outcome.setText("TIE!!!");
+							break;
+						case 1:
+							outcome.setText("P1 Wins!!!");
+							break;
+						case 2:
+							outcome.setText("P2 Wins!!!");
+							break;
+						default:
+							outcome.setText("NOBODY WINS???");
+								
+					}
+					p1Updated = false;
+					p2Updated = false;
+					
+					updateScores(true);
+					
+				}
 				
 			}
 
@@ -207,6 +252,27 @@ public class RPSPanel extends JPanel
 		blue = (int) (Math.random() * 256);
 		
 		this.setBackground(new Color(red, green, blue));
+	}
+	
+	private void updateScores(boolean multiplayer)
+	{
+		int[] scoreBoard;
+		scoreBoard = baseController.getScoreBoard(multiplayer);
+		String p1 = "P1: " + Integer.toString(scoreBoard[0]);
+		p1Score.setText(p1);
+		String tie = "Ties: " + Integer.toString(scoreBoard[1]);
+		tieScore.setText(tie);
+		if(multiplayer)
+		{
+			String p2 = "P2: " + Integer.toString(scoreBoard[2]);
+			p2Score.setText(p2);
+		}
+		else
+		{
+			String bot = "Bot: " + Integer.toString(scoreBoard[2]);
+			p2Score.setText(bot);
+		}
+		
 	}
 	
 	private void updateSelections()
